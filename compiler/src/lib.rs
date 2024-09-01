1af98449d2c8be074163de84b8cdcc08ed3a8831
Samantha <trackmenot10@gmail.com>
2024-08-31 18:19:50 -0700

    Test with `--with-output` instead of main.rs

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -6,12 +6,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_structs() {
+    fn test_debug() {
         let my_tok = Token{
             kind: TokenKind::Dollar,
             len: 1
         };
-        println!("{:?}", my_tok);
+        assert_eq!(format!("{:?}", my_tok), "Token { kind: \"$\", len: 1 }");
 
     }
 }
--- compiler/src/main.rs
+++
@@ -1,9 +0,0 @@
-use gock_compiler::token::*;
-
-fn main() {
-    let my_tok = Token{
-        kind: TokenKind::Dollar,
-        len: 1
-    };
-    println!("{:?}", my_tok);
-}
