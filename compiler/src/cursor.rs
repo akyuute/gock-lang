db3c2475ef3d70600f48b2ed71234250ef43a513
Samantha <trackmenot10@gmail.com>
2024-08-31 18:21:44 -0700

    Impl `Cursor` in cursor.rs

--- compiler/src/cursor.rs
+++ compiler/src/cursor.rs
@@ -1,5 +1,6 @@
 use std::str::Chars;
 
+use crate::token::{Token, TokenKind}
 pub(crate) const EOF_CHAR: char = '\0';
 
 pub struct Cursor {
@@ -10,5 +11,47 @@ pub struct Cursor {
 
 }
 
-impl Cursor {}
+impl Cursor {
+    pub fn advance_token(&mut self) -> Token {
+        let first_char = match self.bump() {
+            Some(c) => c,
+            None => return Token::new(TokenKind::Eof, 0),
+        };
 
+        let token_kind = match first_char {
+
+            '"' => TokenKind::DoubleQuote,
+            '\'' => TokenKind::SingleQuote,
+            '`' => TokenKind::Tick,
+            '(' => TokenKind::OpenPar,
+            ')' => TokenKind::ClosePar,
+            '[' => TokenKind::OpenSqb,
+            ']' => TokenKind::CloseSqb,
+            '{' => TokenKind::OpenBrace,
+            '}' => TokenKind::CloseBrace,
+            ':' => TokenKind::Colon,
+            ',' => TokenKind::Comma,
+            ';' => TokenKind::Semi,
+            '+' => TokenKind::Plus,
+            '-' => TokenKind::Minus,
+            '*' => TokenKind::Star,
+            '/' => TokenKind::Slash,
+            '%' => TokenKind::Percent,
+            '!' => TokenKind::Bang,
+            '|' => TokenKind::Pipe,
+            '&' => TokenKind::Amper,
+            '<' => TokenKind::Less,
+            '>' => TokenKind::Greater,
+            '=' => TokenKind::Equal,
+            '.' => TokenKind::Dot,
+            '~' => TokenKind::Tilde,
+            '^' => TokenKind::Caret,
+            '?' => TokenKind::Question,
+            '#' => TokenKind::Pound,
+            '$' => TokenKind::Dollar,
+            '@' => TokenKind::At,
+            _ => TokenKind::Unknown,
+
+        }
+    }
+}
--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -2,47 +2,4 @@ pub use crate::cursor::Cursor;
 use crate::cursor::EOF_CHAR;
 use crate::token::{Token, TokenKind,};
 
-impl Cursor {
-    pub fn advance_token(&mut self) -> Token {
-        let first_char = match self.bump() {
-            Some(c) => c,
-            None => return Token::new(TokenKind::Eof, 0),
-        };
-
-        let token_kind = match first_char {
-
-            '"' => TokenKind::DoubleQuote,
-            '\'' => TokenKind::SingleQuote,
-            '`' => TokenKind::Tick,
-            '(' => TokenKind::OpenPar,
-            ')' => TokenKind::ClosePar,
-            '[' => TokenKind::OpenSqb,
-            ']' => TokenKind::CloseSqb,
-            '{' => TokenKind::OpenBrace,
-            '}' => TokenKind::CloseBrace,
-            ':' => TokenKind::Colon,
-            ',' => TokenKind::Comma,
-            ';' => TokenKind::Semi,
-            '+' => TokenKind::Plus,
-            '-' => TokenKind::Minus,
-            '*' => TokenKind::Star,
-            '/' => TokenKind::Slash,
-            '%' => TokenKind::Percent,
-            '!' => TokenKind::Bang,
-            '|' => TokenKind::Pipe,
-            '&' => TokenKind::Amper,
-            '<' => TokenKind::Less,
-            '>' => TokenKind::Greater,
-            '=' => TokenKind::Equal,
-            '.' => TokenKind::Dot,
-            '~' => TokenKind::Tilde,
-            '^' => TokenKind::Caret,
-            '?' => TokenKind::Question,
-            '#' => TokenKind::Pound,
-            '$' => TokenKind::Dollar,
-            '@' => TokenKind::At,
-            _ => TokenKind::Unknown,
-
-        }
-    }
-}
+pub struct Lexer{}
